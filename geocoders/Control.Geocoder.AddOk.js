decad5654cc0345f091d369d29e42c9b026c9a47
Cyrille Giquello <cyrille@giquello.fr>
2015-05-06 22:09:45 +0200

    add reverse geocoding to AddOk

--- geocoders/Control.Geocoder.AddOk.js
+++ geocoders/Control.Geocoder.AddOk.js
@@ -2,7 +2,7 @@
 
 L.Control.Geocoder.AddOk = L.Class.extend({
 	options: {
-		serviceUrl: 'http://api-adresse.data.gouv.fr/search/',
+		serviceUrl: 'http://api-adresse.data.gouv.fr',
 		htmlTemplate: function(r) {
 			var parts = [];
 
@@ -36,7 +36,7 @@ L.Control.Geocoder.AddOk = L.Class.extend({
 			q: query,
 		}, this.options.geocodingQueryParams);
 		var that = this ;
-		L.Control.Geocoder.getJSON(this.options.serviceUrl, params, function(data) {
+		L.Control.Geocoder.getJSON(this.options.serviceUrl+'/search/', params, function(data) {
 			var results = [],
 				i,
 				f,
@@ -76,10 +76,46 @@ L.Control.Geocoder.AddOk = L.Class.extend({
 		return this.geocode(query, cb, context);
 	},
 
-	reverse: function(latLng, cb, context) {
-		// Not yet implemented in Photon
-		// https://github.com/komoot/photon/issues/19
-		cb.call(context, []);
+	reverse: function(location, scale, cb, context) {
+		var params = L.extend({
+			lat: location.lat,
+			lon: location.lng
+		}, this.options.reverseQueryParams);
+		var that = this ;
+		L.Control.Geocoder.getJSON(this.options.serviceUrl+'/reverse/', params, function(data) {
+			var results = [],
+				i,
+				f,
+				c,
+				latLng,
+				extent,
+				bbox;
+			if (data && data.features) {
+				for (i = 0; i < data.features.length; i++) {
+					f = data.features[i];
+					c = f.geometry.coordinates;
+					latLng = L.latLng(c[1], c[0]);
+					extent = f.properties.extent;
+
+					if (extent) {
+						bbox = L.latLngBounds([extent[1], extent[0]], [extent[3], extent[2]]);
+					} else {
+						bbox = L.latLngBounds(latLng, latLng);
+					}
+
+					results.push({
+						name: f.properties.name,
+						center: latLng,
+						bbox: bbox,
+						html: that.options.htmlTemplate ?
+							that.options.htmlTemplate(f.properties)
+							: undefined
+					});
+				}
+			}
+
+			cb.call(context, results);
+		});
 	}
 });
 
